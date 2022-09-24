61f7592f8ee0cf8552d3092326701a1c47647713
Jose Kristian Resabal <jkresabal@up.edu.ph>
2022-09-24 20:29:00 +0800

    Added initial code snippets to index.js

---
+++ index.js
@@ -0,0 +1,3 @@
+document.getElementsByName("comet_activity_log_select_all_checkbox")[0].parentElement.parentElement.click(); 
+document.querySelector('div[aria-label="Trash"]').click();
+document.querySelector('div[aria-label="Move to trash"]').click(); 
